dd45396a17e7127fafdef55550ef0d5d9b79c253
Kun Luo <kun.luo@fundamentalinteraction.com>
2013-09-24 14:04:50 -0400

    layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,29 +4,29 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content ('Sample App')
     end
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector ('h1', :text => 'Sample App')
     end
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text=>"Ruby on Rails|Home")
     end
   end
 
   describe "Help Page" do
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_content('Help')
   	end
   end
 
   describe "About Page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About this')
     end
   end
